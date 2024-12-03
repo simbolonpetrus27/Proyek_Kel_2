dc1cf5477b015006a1735efe44a29b56204cf258
simbolonpetrus27 <simbolonpetrus707@gmail.com>
2024-12-03 19:28:20 +0700

    menambahkan beberapa perubahan style

--- src/components/ContohSoalMatriks.js
+++ src/components/ContohSoalMatriks.js
@@ -75,6 +75,74 @@ function ContohSoalMatriks() {
           Hitunglah invers dari matriks A!
         </p>  
       </section>
+      <br />
+
+      {/* Soal Baru */}
+      <section className="soal">
+        <h3>Soal 6: Matriks Pangkat</h3>
+        <p>
+          Diketahui matriks A:
+          <br />
+          <strong>A = </strong> 
+          <em>[ 1  0;  0  1 ]</em>
+          <br />
+          Hitunglah A^5 (A dipangkatkan 5)!
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+        <h3>Soal 7: Matriks Baris dan Kolom</h3>
+        <p>
+          Diketahui matriks A:
+          <br />
+          <strong>A = </strong> 
+          <em>[ 2  5  1;  3  4  6 ]</em>
+          <br />
+          Tentukan jumlah baris dan kolom dari matriks A!
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+        <h3>Soal 8: Matriks Identitas</h3>
+        <p>
+          Diketahui matriks A:
+          <br />
+          <strong>A = </strong> 
+          <em>[ 1  0  0;  0  1  0;  0  0  1 ]</em>
+          <br />
+          Tentukan apakah matriks A adalah matriks identitas!
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+        <h3>Soal 9: Matriks dan Sistem Persamaan Linear</h3>
+        <p>
+          Diketahui sistem persamaan linear berikut:
+          <br />
+          <strong>2x + 3y = 5</strong>
+          <br />
+          <strong>4x - y = 3</strong>
+          <br />
+          Tulis sistem persamaan ini dalam bentuk matriks dan tentukan solusi untuk x dan y!
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+        <h3>Soal 10: Matriks Augmentasi</h3>
+        <p>
+          Diketahui sistem persamaan linear berikut:
+          <br />
+          <strong>x + 2y = 4</strong>
+          <br />
+          <strong>3x - y = 5</strong>
+          <br />
+          Tentukan matriks augmentasi dari sistem persamaan ini!
+        </p>
+      </section>
     </div>
   );
 }
