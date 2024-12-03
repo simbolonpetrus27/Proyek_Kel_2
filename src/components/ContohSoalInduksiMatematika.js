ba3aab31063ec4befe233171691035300b70f472
yohanagtha <yohannaagtha@gmail.com>
2024-12-03 16:50:38 +0700

    menambahkan soal dan contoh soal induksi matematika

--- src/components/ContohSoalInduksiMatematika.js
+++ src/components/ContohSoalInduksiMatematika.js
@@ -36,6 +36,67 @@ function ContohSoalInduksiMatematika() {
         habis dibagi 5. 
         </p>
       </section>
+      <br />
+
+      <section className="soal">
+        <h3>Soal 04</h3>
+        <p>
+        <em>Buktikan 2 + 4 + 6 + … + 2n = n(n + 1), untuk setiap n bilangan asli.</em>
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+        <h3>Soal 05</h3>
+        <p>
+        <em>Gunakanlah induksi matematika untuk membuktikan persamaan</em>
+        <br />
+        <em>Sn = 1 + 3 + 5 +7 +…+ (2n-1) = n<sup>2</sup> untuk semua bilangan bulat n ≥ 1.</em>
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+        <h3>Soal 06</h3>
+        <p>
+        <em>Buktikan 1 + 3 + 5 + … + (2n - 1) = n<sup>2</sup> benar, untuk setiap n bilangan asli</em>
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+        <h3>Soal 07</h3>
+        <p>
+        <em>Buktikan n<sup>3</sup> + 2n habis dibagi 3, untuk setiap n bilangan asli</em>
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+        <h3>Soal 08</h3>
+        <p>
+        <em>Buktikan dengan menggunakan induksi matematika bahwa untuk setiap bilangan bulat positif n, 4<sup>n</sup> - 1 dapat dibagi dengan 3.</em>
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+        <h3>Soal 09</h3>
+        <p>
+        <em>Buktikan dengan menggunakan induksi matematika bahwa untuk setiap bilangan bulat positif n, 9<sup>n</sup> - 1 dapat dibagi dengan 8.</em>
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+        <h3>Soal 10</h3>
+        <p>
+        <em>Buktikan dengan menggunakan induksi matematika bahwa untuk setiap bilangan bulat positif n, 3<sup>n</sup> + 5<sup>n</sup> - 2 adalah bilangan bulat yang dapat dibagi oleh 6.</em>
+        </p>
+      </section>
+      <br />
+
+
     </div>
   );
 }
--- src/components/PembahasanInduksiMatematika.js
+++ src/components/PembahasanInduksiMatematika.js
@@ -171,6 +171,299 @@ function PembahasanInduksiMatematika() {
           <strong>~</strong> -(3<sup>2k</sup>) + 2<sup>2k+2</sup> sama dengan langkah 2, habis dibagi 5
         </p>
       </section>
+      <br />
+
+      <section className="soal">
+      <h3>Soal 04</h3>
+        <p>
+        <em>Buktikan 2 + 4 + 6 + … + 2n = n(n + 1), untuk setiap n bilangan asli.</em>
+        </p>
+        <br />
+
+        <h4>Pembahasan:</h4>
+        <p>
+          <strong>Langkah 1</strong>
+          <br />
+          <em>Akan ditunjukkan n=(1) benar</em>
+          <br />
+          <em>2 = 1(1 + 1)</em>
+          <br />
+          <em>Jadi, P(1) benar</em>
+          <br />
+          <strong>Langkah 2</strong>
+          <br />
+          <em>Akan ditunjukkan n=(k + 1) juga benar, yaitu</em>
+          <br />
+          <em>2 + 4 + 6 + … + 2k + 2(k + 1) = (k + 1)(k + 1 + 1)</em>
+          <br />
+          <em>Dari asumsi :</em>
+          <br />
+          <em>2 + 4 + 6 + … + 2k = k(k + 1)</em>
+          <br />
+          <em>Tambahkan kedua ruas dengan u<sub>k+1</sub> :</em>
+          <br />
+          <em>2 + 4 + 6 + … + 2k + 2(k + 1) = k(k + 1) + 2(k + 1)</em> 
+          <br />
+          <em>2 + 4 + 6 + … + 2k + 2(k + 1) = (k + 1)(k + 2)</em> 
+          <br />
+          <em>2 + 4 + 6 + … + 2k + 2(k + 1) = (k + 1)(k + 1 + 1)</em> 
+          <br />
+          <em>Jadi, n = (k + 1) <strong>benar</strong></em>
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+      <h3>Soal 05</h3>
+        <p>
+        <em>Gunakanlah induksi matematika untuk membuktikan persamaan</em>
+        <br />
+        <em>Sn = 1 + 3 + 5 +7 +…+ (2n-1) = n<sup>2</sup> untuk semua bilangan bulat n ≥ 1.</em>
+        </p>
+        <br />
+
+        <h4>Pembahasan:</h4>
+        <p>
+          <strong>Langkah 1</strong>
+          <br />
+          <em>Akan ditunjukkan n=(1) benar </em>
+          <br />
+          <em>S1 = 1 = 1<sup>2</sup>  </em>
+          <br />
+          <strong>Langkah 2</strong>
+          <br />
+          <em>Asumsikan bahwa n=(k) benar, yaitu </em>
+          <br />
+          <em> 1 + 3 + 5 +7 +...+ 2(k)-1 = k<sup>2</sup> </em>
+          <br />
+          <em>1 + 3 + 5 +7 +...+ (2k-1) = k<sup>2</sup></em>
+          <hr />
+          <strong>Langkah 3</strong>
+          <br />
+          <em>Buktikan bahwa n=(k+1) adalah benar</em>
+          <br />
+          <em>1 + 3 + 5 +7 +...+ (2k-1) + [2(k+1) - 1] = (k+1)<sup>2</sup></em>
+          <br />
+          <em>ingat bahwa 1 + 3 + 5 +7 +...+ (2k-1) = k<sup>2</sup> </em> 
+          <br />
+          <em>maka</em> 
+          <br />
+          <em> k<sup>2</sup> +  [2(k+1) - 1] = (k+1)<sup>2</sup> </em> 
+          <br />
+          <em>k<sup>2</sup> +  2k + 1 = (k+1)<sup>2</sup>  </em>
+          <br />
+          <em> (k+1)2   =   (k+1)2   </em> 
+          <br />
+          <em>maka persamaan di atas terbukti</em> 
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+      <h3>Soal 06</h3>
+        <p>
+        <em>Buktikan 1 + 3 + 5 + … + (2n - 1) = n<sup>2</sup> benar, untuk setiap n bilangan asli</em>
+        </p>
+        <br />
+
+        <h4>Pembahasan:</h4>
+        <p>
+          <strong>Langkah 1</strong>
+          <br />
+          <em>Akan ditunjukkan n=(1) benar</em>
+          <br />
+          <em>1 = 1<sup>2</sup></em>
+          <br />
+          <em>Jadi, P(1) benar</em>
+          <br />
+          <strong>Langkah 2</strong>
+          <br />
+          <em>Asumsikan n=(k) benar, yaitu</em>
+          <br />
+          <em>1 + 3 + 5 + … + (2k - 1) = k<sup>2</sup>,    k ∈ N </em>
+          <br />
+          <strong>Langkah 3</strong>
+          <br />
+          <em>Akan ditunjukkan n=(k + 1) juga benar, yaitu</em>
+          <br />
+          <em>1 + 3 + 5 + … + (2k - 1) + (2(k + 1) - 1) = (k + 1)<sup>2</sup></em>
+          <br />
+          <em>Dari asumsi :</em> 
+          <br />
+          <em>1 + 3 + 5 + ... + (2k - 1) = k<sup>2</sup></em> 
+          <br />
+          <em>Tambahkan kedua ruas dengan u<sub>k+1</sub> :</em> 
+          <br />
+          <em>1 + 3 + 5 + ... + (2k - 1) + (2(k + 1) - 1) = k2 + (2(k + 1) - 1)</em>
+          <br />
+          <em>1 + 3 + 5 +...+ (2k - 1) + (2(k + 1) - 1) = k2 + 2k +1</em>
+          <br />
+          <em>1 + 3 + 5 + ... + (2k - 1) + (2(k + 1) - 1) = (k + 1)2</em>
+          <br />
+          <em>Jadi, n=(k + 1) juga benar</em>
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+      <h3>Soal 07</h3>
+        <p>
+        <em>Buktikan n<sup>3</sup> + 2n habis dibagi 3, untuk setiap n bilangan asli</em>
+        </p>
+        <br />
+
+        <h4>Pembahasan:</h4>
+        <p>
+          <strong>Langkah 1</strong>
+          <br />
+          <em>Akan ditunjukkan n=(1) benar</em>
+          <br />
+          <em>1<sup>3</sup> + 2.1 = 3 = 3.1</em>
+          <br />
+          <em>Jadi, n=(1) benar</em>
+          <br />
+          <strong>Langkah 2</strong>
+          <br />
+          <em>Asumsikan n=(k) benar, yaitu</em>
+          <br />
+          <em>k<sup>3</sup> + 2k = 3m,    k ∈ NN</em>
+          <br />
+          <strong>Langkah 3</strong>
+          <br />
+          <em>Akan ditunjukkan n=(k + 1) juga benar, yaitu</em>
+          <br />
+          <em>(k + 1)<sup>3</sup> + 2(k + 1) = 3p,     p ∈ ZZ</em>
+          <br />
+          <em>(k + 1)<sup>3</sup> + 2(k + 1) = (k<sup>3</sup> + 3k<sup>2</sup> + 3k + 1) + (2k + 2)</em>
+          <br />
+          <em>(k + 1)<sup>3</sup> + 2(k + 1) = (k<sup>3</sup> + 2k) + (3k<sup>2</sup> + 3k + 3)  </em>
+          <br />
+          <em>(k + 1)<sup>3</sup> + 2(k + 1) = 3m + 3(k<sup>2</sup> + k + 1)</em>
+          <br />
+          <em>(k + 1)<sup>3</sup> + 2(k + 1) = 3(m + k<sup>2</sup> + k + 1)</em>
+          <br />
+          <em>Karena m bilangan bulat dan k bilangan asli, maka (m + k<sup>2</sup> + k + 1) adalah bilangan bulat.</em>
+          <br />
+          <em>Misalkan p = (m + k<sup>2</sup> + k + 1), maka</em>
+          <br />
+          <em>(k + 1)<sup>3</sup> + 2(k + 1) = 3p, dengan p ∈ ZZ</em>
+          <br />
+          <em>Jadi, n = (k + 1) <strong>benar</strong></em>
+          <br />
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+      <h3>Soal 08</h3>
+        <p>
+        <em>Buktikan dengan menggunakan induksi matematika bahwa untuk setiap bilangan bulat positif n, 4<sup>n</sup> - 1 dapat dibagi dengan 3.</em>
+        </p>
+        <br />
+
+        <h4>Pembahasan:</h4>
+        <p>
+          <strong>Langkah 1</strong>
+          <br />
+          <em>Basis Induksi (n=1):</em>
+          <br />
+          <em>4<sup>1</sup> - 1 = 4 - 1 = 3, yang dapat dibagi dengan 3.</em>
+          <br />
+          <strong>Langkah 2</strong>
+          <br />
+          <em>Langkah Induksi (asumsi n=k):</em>
+          <br />
+          <em>4<sup>k</sup> - 1 dapat dibagi dengan 3.</em>
+          <br />
+          <strong>Langkah 3</strong>
+          <br />
+          <em>Langkah Induksi (n=k+1):</em>
+          <br />
+          <em>Buktikan untuk n=k+1:</em>
+          <br />
+          <em>4<sup>(k+1)</sup> - 1 = 4 * 4<sup>k</sup> - 1 = 4(4<sup>k</sup> - 1) + 3.</em>
+          <br />
+          <em>Dalam kasus ini, kita menggunakan asumsi induksi (4<sup>k</sup> - 1 dapat dibagi dengan 3). Oleh karena itu, 4<sup>k</sup> - 1 dapat ditulis sebagai 3m (dengan m adalah suatu bilangan bulat). Jadi, 4<sup>(k+1)</sup> - 1 = 4 * 3m + 3 = 3(4m + 1).</em>
+          <br />
+          <em>Karena 4m + 1 juga merupakan bilangan bulat, kita bisa menyatakan 4<sup>(k+1)</sup> - 1 sebagai kelipatan dari 3. Dengan demikian, induksi untuk n=k+1 terbukti.</em>
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+      <h3>Soal 09</h3>
+        <p>
+        <em>Buktikan dengan menggunakan induksi matematika bahwa untuk setiap bilangan bulat positif n, 9<sup>n</sup> - 1 dapat dibagi dengan 8.</em>
+        </p>
+        <br />
+
+        <h4>Pembahasan:</h4>
+        <p>
+          <strong>Langkah 1</strong>
+          <br />
+          <em>Basis Induksi (n=1):</em>
+          <br />
+          <em>9<sup>k</sup> - 1 = 9 - 1 = 8, yang dapat dibagi dengan 8.</em>
+          <br />
+          <strong>Langkah 2</strong>
+          <br />
+          <em>Langkah Induksi (asumsi n=k):</em>
+          <br />
+          <em>9<sup>k</sup> - 1 dapat dibagi dengan 8.</em>
+          <br />
+          <strong>Langkah 3</strong>
+          <br />
+          <em>ALangkah Induksi (n=k+1):</em>
+          <br />
+          <em>Buktikan untuk n=k+1:</em>
+          <br />
+          <em>Dengan asumsi induksi (9<sup>k</sup> - 1 dapat dibagi dengan 8), kita tulis 9<sup>k</sup> - 1 sebagai 8m (dengan m adalah suatu bilangan bulat). 
+          Jadi, 9<sup>(k+1)</sup> - 1 = 9 * 8m + 8 = 8(9m + 1).</em>
+          <br />
+          <em>Karena 9m + 1 juga merupakan bilangan bulat, kita bisa menyatakan 9<sup>(k+1)</sup> - 1 sebagai kelipatan dari 8. Dengan demikian, induksi untuk n=k+1 terbukti.</em>
+        </p>
+      </section>
+      <br />
+
+      <section className="soal">
+      <h3>Soal 10</h3>
+        <p>
+        <em>Buktikan dengan menggunakan induksi matematika bahwa untuk setiap bilangan bulat positif n, 3<sup>n</sup> + 5<sup>n</sup> - 2 adalah bilangan bulat yang dapat dibagi oleh 6.</em>
+        </p>
+        <br />
+
+        <h4>Pembahasan:</h4>
+        <p>
+          <strong>Langkah 1</strong>
+          <br />
+          <em>Basis Induksi (n=1):</em>
+          <br />
+          <em>3<sup>1</sup> + 5<sup>1</sup> - 2 = 3 + 5 - 2 = 6, yang dapat dibagi dengan 6.</em>
+          <br />
+          <strong>Langkah 2</strong>
+          <br />
+          <em>Langkah Induksi (asumsi n=k):</em>
+          <br />
+          <em>3<sup>k</sup> + 5<sup>k</sup> - 2 adalah bilangan bulat yang dapat dibagi oleh 6.</em>
+          <br />
+          <strong>Langkah 3</strong>
+          <br />
+          <em>Langkah Induksi (n=k+1):</em>
+          <br />
+          <em>Buktikan untuk n=k+1:</em>
+          <br />
+          <em>3<sup>(k+1)</sup> + 5<sup>(k+1)</sup> - 2 adalah bilangan bulat yang dapat dibagi oleh 6.</em>
+          <br />
+          <em>Kita gunakan asumsi induksi (3<sup>k</sup> + 5<sup>k</sup> - 2 adalah bilangan bulat yang dapat dibagi oleh 6). </em>
+          <br />
+          <em>3<sup>(k+1)</sup> + 5<sup>(k+1)</sup> - 2 = 3 * 3<sup>k</sup> + 5 * 5<sup>k</sup> - 2   = 3 * (3<sup>k</sup> + 5<sup>k</sup> - 2) + 2 * 5<sup>k</sup>.</em>
+          <br />
+          <em>Karena 3<sup>k</sup> + 5<sup>k</sup> - 2 adalah bilangan bulat yang dapat dibagi oleh 6 berdasarkan asumsi induksi, dan 2 * 5<sup>k</sup> adalah bilangan bulat, maka 3<sup>(k+1)</sup> + 5<sup>(k+1)</sup> - 2 juga adalah bilangan bulat yang dapat dibagi oleh 6. 
+          Induksi untuk n=k+1 terbukti.</em>
+        </p>
+      </section>
+      <br />
+
     </div>
   );
 }
